ff02d901c1e86e3454818542639d742c4a97d2cd
CheRongtian <145754269+CheRongtian@users.noreply.github.com>
2025-11-25 00:04:00 -0600

    add a missing return

--- B-S Formula/Random1.cpp
+++ B-S Formula/Random1.cpp
@@ -9,6 +9,7 @@ double GetOneGaussianBySummation()
     double result = 0;
     for(unsigned long j=0; j<12; j++) result += rand()/static_cast<double> (RAND_MAX);
     result -= 6.0;
+    return result;
 }
 
 double GetOneGaussianByBoxMuller()
